db80cf5910eb7d0201871cede15c65278863e3a7
Jeevika11105 <146510019+Jeevika11105@users.noreply.github.com>
2026-04-12 11:35:37 +0530

    Time: 1308 ms (38.93%), Space: 0B (100%) - LeetHub

---
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -0,0 +1,17 @@
+# Write your MySQL query statement below
+(SELECT name AS resultS
+FROM Users
+INNER JOIN MovieRating USING(user_id)
+GROUP BY user_id
+ORDER BY COUNT(*) DESC, name ASC
+LIMIT 1)
+
+UNION ALL
+
+(SELECT title AS results 
+FROM Movies
+INNER JOIN MovieRating USING(movie_id)
+WHERE MONTH(created_at) = '02' AND YEAR(created_at) = '2020'
+GROUP BY title
+ORDER BY AVG(rating) DESC, title ASC
+LIMIT 1);
